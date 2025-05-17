ccf4760c78a01f609d569fa351cd14d94e2693e6
Hwanseo Choi <electricvoxel@gmail.com>
2025-05-17 15:03:07 -0700

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
-npx --yes @tailwindcss/cli -i ./templates/tailwind.scss -o ./content/style.css --minify
+if [ ! -f temp/tailwindcss-linux-x64 ]; then
+  curl -sL https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64 -o temp/tailwindcss-linux-x64
+  chmod a+x temp/tailwindcss-linux-x64
+fi
+./temp/tailwindcss-linux-x64 -i ./templates/tailwind.css -o ./content/tailwind.css
 
 python3 gh-md-to-html.py
