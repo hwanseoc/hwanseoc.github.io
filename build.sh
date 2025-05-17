f6167801f194c5b9c780446ccc2756671a0997f3
Hwanseo Choi <electricvoxel@gmail.com>
2025-05-17 15:57:29 -0700

    minify

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ if [ ! -f temp/tailwindcss-linux-x64 ]; then
   curl -sL https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64 -o temp/tailwindcss-linux-x64
   chmod a+x temp/tailwindcss-linux-x64
 fi
-./temp/tailwindcss-linux-x64 -i ./templates/tailwind.css -o ./content/tailwind.css
+./temp/tailwindcss-linux-x64 -i ./templates/tailwind.css -o ./content/tailwind.css --minify
 
 python3 gh-md-to-html.py
